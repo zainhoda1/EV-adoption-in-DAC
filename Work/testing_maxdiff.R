06844fe9af499363853a5019e721bd50f0d9dd1c
Zain Hoda <zainhoda1@gmail.com>
2024-02-20 14:45:48 -0500

    Progress till week 4

---
+++ Work/testing_maxdiff.R
@@ -0,0 +1,15 @@
+#install.packages("remotes")
+#remotes::install_github("erikerhardt/flipMaxDiff")
+
+#pkgbuild::check_build_tools(debug = TRUE)
+
+
+#install.packages("bwsTools")
+
+library(bwsTools)
+library(dplyr)
+library(tidyr)
+
+bibds
+
+a <- make_bibd(29, seed = 1839)
